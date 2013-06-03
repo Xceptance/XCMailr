ad3da865f83e7636d4c9a7a4d1a58b9555fcb72d
Patrick Thum <p.thum@xceptance.net>
2013-06-03 11:45:03 +0000

    externalized logging (issue #1822)

--- xcmailr-resources/run.sh
+++ xcmailr-resources/run.sh
@@ -2,11 +2,12 @@
 
 #setup paths and options
 CURR_DIR=`pwd`
-JVM_OPTS=-Dsun.lang.ClassLoader.allowArraySyntax=true 
+JVM_OPTS="-Dsun.lang.ClassLoader.allowArraySyntax=true "
 XCMAILR_OPTS="-Dxcmailr.xcmstarter.home=$CURR_DIR -Dxcmailr.xcmstarter.host=localhost -Dxcmailr.xcmstarter.port=8080 -Dninja.external.configuration=conf/application.conf -Dninja.mode=prod $@"
-JETTY_OPTS=-Dorg.eclipse.jetty.util.log.stderr.DEBUG=true
-XCMAILR_JAR=./xcmailr-jetty-starter.jar
+LOGBK_OPTS="-Dlogback.configurationFile=conf/logback.xml"
+JETTY_OPTS="-Dorg.eclipse.jetty.util.log.stderr.DEBUG=true"
+XCMAILR_JAR="./xcmailr-jetty-starter.jar"
 
 # run with all options
-java $JVM_OPTS $XCMAILR_OPTS $JETTY_OPTS -jar $XCMAILR_JAR
+java $JVM_OPTS $XCMAILR_OPTS $LOGBK_OPTS $JETTY_OPTS -jar $XCMAILR_JAR
 
