4b4fa37c098488fae215137fcfeae79a576cfdf0
Patrick Thum <p.thum@xceptance.net>
2013-06-18 11:50:33 +0000

    #1903: fixed the handling to edit "unlimited"-mailboxes

--- xcmailr-webapp/src/main/java/assets/js/picker-enable.js
+++ xcmailr-webapp/src/main/java/assets/js/picker-enable.js
@@ -3,31 +3,27 @@
  * Patrick Thum, Xceptance Software Technologies GmbH, 2013
  */
 
-	function toggle(chkbox) {
-		var picker = $('#datetimepicker')
-				.data('datetimepicker');
-		var divDateTimePicker = chkbox.parentNode;
-		var visSetting = (chkbox.checked) ? "display: none" : "display: inline-block";
-		$('input#datetime').attr("style", visSetting);
-		$('span#pickr_span').attr("style", visSetting);
-		$('i#pickr_i').attr("style", visSetting);
+function toggle(chkbox) {
+	var picker = $('#datetimepicker')
+			.data('datetimepicker');
+	var divDateTimePicker = chkbox.parentNode;
+	var visSetting = (chkbox.checked) ? "display: none" : "display: inline-block";
+	$('input#datetime').attr("style", visSetting);
+	$('span#pickr_span').attr("style", visSetting);
+	$('i#pickr_i').attr("style", visSetting);
 
-	/*	divDateTimePicker.getElementsByTagName('input')[0].style.display = visSetting;
-		divDateTimePicker.getElementsByTagName('span')[0].style.display = visSetting;
-		divDateTimePicker.getElementsByTagName('i')[0].style.display = visSetting;*/
-		if (chkbox.checked) {
-			var dateval = $('input#datetime').attr("value");
-			//remove the attribute from datetime input field
-			$('input#datetime').attr("value", 0);
-			//add it to the checkbox 
-			$('input#chkUnlimited').attr("value", dateval);
+	if (chkbox.checked) {
+		var dateval = $('input#datetime').attr("value");
+		//remove the attribute from datetime input field
+		$('input#datetime').attr("value", 0);
+		//add it to the checkbox 
+		$('input#chkUnlimited').attr("value", dateval);
 
-		} else {
-			var dateval = $('input#chkUnlimited').attr("value");
-			//add the attribute to datetime input field 
-			$('input#datetime').attr("value", dateval);
-			//remove it from the checkbox 
-			$('input#chkUnlimited').removeAttr("value");
-		}
-
+	} else {
+		var dateval = $('input#chkUnlimited').attr("value");
+		//add the attribute to datetime input field 
+		$('input#datetime').attr("value", dateval);
+		//remove it from the checkbox 
+		$('input#chkUnlimited').removeAttr("value");
 	}
+}
