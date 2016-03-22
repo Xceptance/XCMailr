13a3a4b689e593d7bf73fff0a1313dec5a2b3fa9
hoelbing <carsten@hoelbing.net>
2016-03-22 17:54:43 +0100

    Testsuite: minor changes, add testcast (TSearch_NoResults)

---
+++ xcmailr_testsuite/src/tests/search/TSearch_NoResults.java
@@ -0,0 +1,26 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package tests.search;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * <p>Test search for no email found.</p>
+ * <h1 id="1-setup-and-preparation">1. Setup and preparation</h1>
+ * <ul>
+ * <li>Start session, open homepage and delete all visible cookies</li>
+ * <li>Create user</li>
+ * </ul>
+ * <h1 id="2-scope-of-test">2. Scope of test</h1>
+ * <ul>
+ * <li>Search for text</li>
+ * <li>Validate empty search result</li>
+ * </ul>
+ */
+@ScriptName
+("tests.search.TSearch_NoResults")
+public class TSearch_NoResults extends AbstractScriptTestCase
+{
+}
