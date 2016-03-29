d75c8476553ee4efc541f112ee469fc56e18d298
hoelbing <carsten@hoelbing.net>
2016-03-29 09:55:12 +0200

    Testsuite: rename testcase, minor changes
    Signed-off-by: hoelbing <carsten@hoelbing.net>

--- xcmailr_testsuite/src/tests/search/TSearch_oneEmail.java
+++ xcmailr_testsuite/src/tests/search/TSearch_oneEmail.java
@@ -7,20 +7,19 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * <p>Test search for no email found.</p>
+ * <p>Test search for one email.</p>
  * <h1 id="1-setup-and-preparation">1. Setup and preparation</h1>
  * <ul>
  * <li>Start session, open homepage and delete all visible cookies</li>
- * <li>Create user</li>
  * </ul>
  * <h1 id="2-scope-of-test">2. Scope of test</h1>
  * <ul>
- * <li>Search for text</li>
- * <li>Validate empty search result</li>
+ * <li>Enter exist email </li>
+ * <li>Validate  results page</li>
  * </ul>
  */
 @ScriptName
-("tests.search.TSearch_Results")
-public class TSearch_Results extends AbstractScriptTestCase
+("tests.search.TSearch_oneEmail")
+public class TSearch_oneEmail extends AbstractScriptTestCase
 {
 }
