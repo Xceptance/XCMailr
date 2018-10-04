225c1c61867c3b7575fa19a367d051481cdb4fee
Marcus Kaufmann <m.kaufmann@xceptance.net>
2018-10-04 19:03:30 +0200

    updated DDL scripts

--- xcmailr-resources/default-drop.sql
+++ xcmailr-resources/default-drop.sql
@@ -4,11 +4,13 @@ drop table if exists register_domains;
 
 drop table if exists mailboxes;
 
+drop table if exists mail;
+
+drop table if exists MAIL_STATISTICS;
+
 drop table if exists mailtransactions;
 
 drop table if exists users;
-
-drop table if exists mail_statistics;
 
 SET REFERENTIAL_INTEGRITY TRUE;
 
@@ -16,8 +18,11 @@ drop sequence if exists register_domains_seq;
 
 drop sequence if exists mailboxes_seq;
 
+drop sequence if exists mail_seq;
+
+drop sequence if exists MAIL_STATISTICS_seq;
+
 drop sequence if exists mailtransactions_seq;
 
 drop sequence if exists users_seq;
 
-drop view V_EMAIL_STATISTICS_24;
