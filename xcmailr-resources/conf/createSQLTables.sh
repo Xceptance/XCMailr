b1606d4facd9cd6b4f1476d82359d11c1762d8c7
Marcus Kaufmann <m.kaufmann@xceptance.net>
2018-10-05 14:11:42 +0200

    removed dedicated create tables script

--- xcmailr-resources/conf/createSQLTables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#runs the drop table script
-java -cp ../lib/h2*.jar org.h2.tools.RunScript -user sa -url jdbc:h2:~/testDB -script default-drop.sql
-
-#runs the create table script
-java -cp ../lib/h2*.jar org.h2.tools.RunScript -user sa -url jdbc:h2:~/testDB -script default-create.sql
