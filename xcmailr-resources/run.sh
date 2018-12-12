f54dc0bc586dfdfe7bdf7bf34acc00f65ea38cfe
Hartmut Arlt <h.arlt@xceptance.net>
2018-12-12 12:31:01 +0100

    Added H2 property 'maxCompactTime' to XCMailr options.

--- xcmailr-resources/run.sh
+++ xcmailr-resources/run.sh
@@ -3,7 +3,7 @@
 #setup paths and options
 CURR_DIR=`dirname $0`
 JVM_OPTS="-Dsun.lang.ClassLoader.allowArraySyntax=true "
-XCMAILR_OPTS="-Dxcmailr.xcmstarter.home=$CURR_DIR -Dninja.external.configuration=conf/application.conf -Dninja.mode=prod $@"
+XCMAILR_OPTS="-Dh2.maxCompactTime=86400000 -Dxcmailr.xcmstarter.home=$CURR_DIR -Dninja.external.configuration=conf/application.conf -Dninja.mode=prod $@"
 LOGBK_OPTS="-Dlogback.configurationFile=conf/logback.xml"
 JETTY_OPTS="-Djetty.logging=$CURR_DIR/logs/jetty.log"
 XCMAILR_JAR="xcmailr-jetty-starter.jar"
