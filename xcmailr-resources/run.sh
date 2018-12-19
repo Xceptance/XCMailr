57b3825589b9a4df633d45657f2f19b1aae327ab
Hartmut Arlt <h.arlt@xceptance.net>
2018-12-19 12:37:42 +0100

    XCMailr start scripts - lowered down maximum time allowed for H2DB compacting - made file path of Java keystore and its passphrase configurable (via env variables)

--- xcmailr-resources/run.sh
+++ xcmailr-resources/run.sh
@@ -1,16 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+set -e
+
+KEYSTORE_FILE=${KEYSTORE_FILE:-"conf/keystore.jks"}
+KEYSTORE_PW=${KEYSTORE_PW:-"topsecret"}
+XCM_INSTALL_DIR=${XCM_INSTALL_DIR:-`realpath -m $(dirname $0)/..`}
 
 #setup paths and options
-CURR_DIR=`dirname $0`
 JVM_OPTS="-Dsun.lang.ClassLoader.allowArraySyntax=true"
-JVM_OPTS="$JVM_OPTS -Djavax.net.ssl.keyStore=conf/keystore.jks"
-JVM_OPTS="$JVM_OPTS -Djavax.net.ssl.keyStorePassword=topsecret"
-XCMAILR_OPTS="-Dh2.maxCompactTime=86400000 -Dxcmailr.xcmstarter.home=$CURR_DIR -Dninja.external.configuration=conf/application.conf -Dninja.mode=prod $@"
+JVM_OPTS="$JVM_OPTS -Djavax.net.ssl.keyStore=$KEYSTORE_FILE"
+JVM_OPTS="$JVM_OPTS -Djavax.net.ssl.keyStorePassword=$KEYSTORE_PW"
+XCMAILR_OPTS="-Dh2.maxCompactTime=15000 -Dxcmailr.xcmstarter.home=$XCM_INSTALL_DIR -Dninja.external.configuration=conf/application.conf -Dninja.mode=prod $@"
 LOGBK_OPTS="-Dlogback.configurationFile=conf/logback.xml"
-JETTY_OPTS="-Djetty.logging=$CURR_DIR/logs/jetty.log"
+JETTY_OPTS="-Djetty.logging=$XCM_INSTALL_DIR/logs/jetty.log"
 XCMAILR_JAR="xcmailr-jetty-starter.jar"
 
 # run with all options
-cd $CURR_DIR
+cd $XCM_INSTALL_DIR
 exec java $JVM_OPTS $XCMAILR_OPTS $LOGBK_OPTS $JETTY_OPTS -jar $XCMAILR_JAR
-
