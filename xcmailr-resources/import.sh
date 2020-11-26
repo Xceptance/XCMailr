f101a77318344322a469496cc016a4ef8df661b1
Hartmut Arlt <h.arlt@xceptance.net>
2020-11-26 12:02:21 +0100

    Added some utility shell scripts that can be used to - run any SQL statement - export the database to a given file - import a database from a given file

---
+++ xcmailr-resources/import.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+_dir=$(dirname "$0")
+
+if [ $# -lt 1 ]; then
+ echo "Must specify at least one argument"
+ exit 1
+fi
+
+if [ -z "$1" ]; then
+ echo "Empty filename?"
+ exit 1
+fi
+
+IMPORT_FILENAME=$1
+IMPORT_SQL="runscript from '"$IMPORT_FILENAME"';"
+
+sh $_dir/shell.sh "$IMPORT_SQL"
---
+++ xcmailr-resources/shell.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+set -e
+
+_dir=$(dirname "$0")
+
+conf_val() {
+  local raw_val=`cat $_dir/conf/application.conf | egrep "^ebean.datasource.$1"`
+  echo "${raw_val#*=}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'
+}
+
+[ -r $_dir/conf/application.conf ] || {
+  echo "Application configuration file not found"
+  exit 1
+}
+
+DB_URL=`conf_val databaseUrl`
+DB_USER=`conf_val username`
+DB_PASS=`conf_val password`
+
+if [ -z "$DB_URL" ]; then
+  echo "DB url not found or is empty. Please check your configuration."
+  exit 1
+fi
+
+if [ -z "$DB_USER" ]; then
+  echo "DB username not found or is empty. Please check your configuration."
+  exit 1
+fi
+
+ARGS="-url $DB_URL -user $DB_USER"
+if [ -n "$DB_PASS" ]; then
+  ARGS="$ARGS -pass $DB_PASS"
+fi
+
+if [ $# -lt 1 ]; then
+  java -cp lib/h2*.jar org.h2.tools.Shell $ARGS 
+else
+  java -cp lib/h2*.jar org.h2.tools.Shell $ARGS -sql "$1"
+fi
