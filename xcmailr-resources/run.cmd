f5a7577544348a0b3bfd6705ad9edb9be7c1a7b1
Hartmut Arlt <h.arlt@xceptance.net>
2022-12-13 14:02:51 +0100

    Add Java keystore parameters only when keystore file exists; polished run.[sh|cmd] scripts.

--- xcmailr-resources/run.cmd
+++ xcmailr-resources/run.cmd
@@ -1,16 +1,18 @@
 @echo off
 setlocal enabledelayedexpansion
 
-if not defined KEYSTORE_FILE set KEYSTORE_FILE="conf/keystore.jks"
+:: setup paths and options
+if not defined XCM_INSTALL_DIR for %%i in ("%~dp0") do set XCM_INSTALL_DIR=%%~fi
+cd /d "%XCM_INSTALL_DIR%"
+
+if not defined KEYSTORE_FILE set KEYSTORE_FILE=conf/keystore.jks
 if not defined KEYSTORE_PW set KEYSTORE_PW=topsecret
 
-:: setup paths and options
-if not defined XCM_INSTALL_DIR for %%i in ("%~dp0..") do set XCM_INSTALL_DIR=%%~fi
+if exist "%KEYSTORE_FILE%" (
+  set JVM_OPTS=%JVM_OPTS% "-Djavax.net.ssl.keyStore=%KEYSTORE_FILE%" "-Djavax.net.ssl.keyStorePassword=%KEYSTORE_PW%"
+)
 
-set JVM_OPTS=%JVM_OPTS% -Dsun.lang.ClassLoader.allowArraySyntax=true
-set JVM_OPTS=%JVM_OPTS% -Djavax.net.ssl.keyStore=%KEYSTORE_FILE%
-set JVM_OPTS=%JVM_OPTS% -Djavax.net.ssl.keyStorePassword=%KEYSTORE_PW%
-set XCMAILR_OPTS=-Dh2.maxCompactTime=150000 -Dxcmailr.xcmstarter.home=%XCM_INSTALL_DIR% -Dninja.external.configuration=conf/application.conf -Dninja.mode=prod %*
+set XCMAILR_OPTS=-Dh2.maxCompactTime=150000 "-Dxcmailr.xcmstarter.home=%XCM_INSTALL_DIR:\=/%" -Dninja.external.configuration=conf/application.conf -Dninja.mode=prod %*
 set LOGBK_OPTS="-Dlogback.configurationFile=conf/logback.xml"
 set JETTY_OPTS="-Djetty.logging=%XCM_INSTALL_DIR:\=/%/logs/jetty.log"
 set XCMAILR_JAR="./xcmailr-jetty-starter.jar"
@@ -21,6 +23,6 @@ for %%p in (%PACKAGES%) do set JVM_OPTS=!JVM_OPTS! --add-opens=%%p=ALL-UNNAMED
 rem set JVM_OPTS=%JVM_OPTS% --illegal-access=debug
 
 :: run with all options
-cd %XCM_INSTALL_DIR%
 java %JVM_OPTS% %XCMAILR_OPTS% %LOGBK_OPTS% %JETTY_OPTS% -jar %XCMAILR_JAR%
+
 
--- xcmailr-resources/run.sh
+++ xcmailr-resources/run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
 
-KEYSTORE_FILE=${KEYSTORE_FILE:-"conf/keystore.jks"}
+KEYSTORE_FILE=${KEYSTORE_FILE-"conf/keystore.jks"}
 KEYSTORE_PW=${KEYSTORE_PW:-"topsecret"}
 XCM_INSTALL_DIR=${XCM_INSTALL_DIR:-`realpath -m $(dirname $0)`}
 
+cd $XCM_INSTALL_DIR
+
 # setup paths and options
-JVM_OPTS="$JVM_OPTS -Djavax.net.ssl.keyStore=$KEYSTORE_FILE"
-JVM_OPTS="$JVM_OPTS -Djavax.net.ssl.keyStorePassword=$KEYSTORE_PW"
+if [ -f "$KEYSTORE_FILE" ]; then
+  JVM_OPTS="$JVM_OPTS -Djavax.net.ssl.keyStore=$KEYSTORE_FILE -Djavax.net.ssl.keyStorePassword=$KEYSTORE_PW"
+fi
 XCMAILR_OPTS="-Dh2.maxCompactTime=15000 -Dxcmailr.xcmstarter.home=$XCM_INSTALL_DIR -Dninja.external.configuration=conf/application.conf -Dninja.mode=prod $@"
 LOGBK_OPTS="-Dlogback.configurationFile=conf/logback.xml"
 JETTY_OPTS="-Djetty.logging=$XCM_INSTALL_DIR/logs/jetty.log"
@@ -19,5 +22,4 @@ for p in $PACKAGES; do JVM_OPTS="$JVM_OPTS --add-opens=$p=ALL-UNNAMED"; done
 #JVM_OPTS="$JVM_OPTS --illegal-access=debug"
 
 # run with all options
-cd $XCM_INSTALL_DIR
 exec java $JVM_OPTS $XCMAILR_OPTS $LOGBK_OPTS $JETTY_OPTS -jar $XCMAILR_JAR
